f2ac1df5c4df5631b8d705793efeb1f9ee87fcb6
KP <kp@shotgunsoftware.com>
2011-08-02 18:32:03 -0700

    match python output closer to command-line output for consistency cleanup

--- src/io_output.py
+++ src/io_output.py
@@ -2,7 +2,6 @@
 
 import sys
 import logging
-
 try:
     import simplejson as json
 except ImportError:
@@ -17,7 +16,6 @@ except ImportError:
                 "or if you have the setuptools module installed in Python, just type `easy_install simplejson`" % python_version
         raise ImportError(msg)
 
-#@@TODO one class that reads format strings from config files that can be packaged up by product
 
 class OutputDefault (object):
     def __init__(self): 
@@ -25,10 +23,14 @@ class OutputDefault (object):
 
     
     def format_user(self, vals):
-        out = vals
+        out = False
+        if vals:
+            out = {
+                'id': vals.get('id'),
+                'name': vals.get('login')
+                }
 
         return out
-
     
     def format_users(self, vals):
         out = []
@@ -36,17 +38,34 @@ class OutputDefault (object):
             out.append({'id':u['id'], 'name':u['login']})
         return out
 
-
     def format_tasks(self, vals):
         out = []
         for t in vals:
+            project_id = None
+            project_name = ''
+            entity_id = None
+            entity_type = None
+            entity_name = ''
+
+            project = t.get('project')
+            if project:
+                project_id = project.get('id')
+                project_name = project.get('name')
+            entity = t.get('entity')
+            if entity:
+                entity_id = entity.get('id')
+                entity_type = entity.get('type')
+                entity_name = entity.get('name')
+            
             out.append(
                 {
                     'id':t['id'], 
-                    'name':t['content']
+                    'project_id': project_id, 
+                    'entity_type': entity_type, 
+                    'entity_id': entity_id, 
+                    'display_name': "%s | %s | %s" % (project_name, entity_name, t['content'])
                 })
         return out
-
 
     def format_projects(self, vals):
         out = []
@@ -54,90 +73,77 @@ class OutputDefault (object):
             out.append({'id':p['id'], 'name':p['name']})
         return out
 
-
     def format_projectsandtasks(self, vals):
         out = vals
-        
         return out
-
 
     def format_shots(self, vals):
-        out = vals
-        
+        out = []
+        for e in vals:
+            out.append(
+                {
+                    'id': e.get('id'),
+                    'name': e.get('code'),
+                })
         return out
-
 
     def format_assets(self, vals):
-        out = vals
-        
+        out = []
+        for e in vals:
+            out.append(
+                {
+                    'id': e.get('id'),
+                    'name': e.get('code'),
+                })
         return out
-
 
     def format_assetsandshots(self, vals):
-        out = vals
-        
+        out = []
+        for e in vals:
+            out.append(
+                {
+                    'id': e.get('id'),
+                    'name': e.get('code'),
+                    'type': e.get('type'),
+                })
         return out
-
 
     def format_version_fields(self, vals):
         out = vals
-
         return out
-
 
     def format_version_statuses(self, vals):
-        out = vals
-        
+        out = vals       
         return out
-
 
     def format_version_name_templates(self, vals):
-        out = vals
-        
+        out = vals        
         return out
-
 
     def format_thumbnail_id(self, vals):
-        out = vals
-        
+        out = vals        
         return out
-
 
     def format_movie_upload(self, vals):
-        out = vals
-        
+        out = vals        
         return out
-
-
-    def format_scene_path_parse(self, vals):
-        out = vals
-        
-        return out
-
 
     def format_version_create(self, vals):
-        out = vals
-        
+        out = vals       
         return out
-
 
     def format_version_update(self, vals):
-        out = vals
-        
+        out = vals        
         return out
-
 
     def format_output(self, listtype, vals):
         try:
             out = getattr(self, 'format_%s' % listtype)(vals)
         except AttributeError:
-            # don't need to raise an error we're return stuff as-is
+            # don't need to raise an error we'll return stuff as-is
             # raise AttributeError('No output method defined for format_%s()' % listtype)
             return vals
         return out
-
-
-
 
 
 class OutputCmdLine (OutputDefault):
@@ -150,17 +156,13 @@ class OutputCmdLine (OutputDefault):
         out = ""
         if vals:
             out += "%d" % (vals['id'])
-
         return out
-
     
     def format_users(self, vals):
         out = ""
         for u in vals:
             out += "%d %s\n" % (u['id'], u['login'])
-
         return out
-
 
     def format_tasks(self, vals):
         out = ""
@@ -176,46 +178,28 @@ class OutputCmdLine (OutputDefault):
                 e_id = t['entity']['id']
                 e_type = t['entity']['type']
                 e_name = t['entity']['name']
-            out += "%s,%s,%s,%s %s | %s | %s\n" % (t['id'], p_id, e_type, e_id, p_name, e_name, t['content'])
-        
+            out += "%s,%s,%s,%s %s | %s | %s\n" % (t['id'], p_id, e_type, e_id, p_name, e_name, t['content'])       
         return out
-
 
     def format_projects(self, vals):
         out = ""
         for p in vals:
-            out += "%d %s\n" % (p['id'], p['name'])
-        
+            out += "%d %s\n" % (p['id'], p['name'])        
         return out
-
-
-    def format_projectsandtasks(self, vals):
-        out = "output format for 'projectsandtasks' is not yet implemented"
-        # for p in vals:
-        #     out += "%d %s\n" % (p['id'], p['name'])
-        
-        return out
-
 
     def format_shots(self, vals):
         out = ""
         for s in vals:
-            out += "%d %s\n" % (s['id'], s['code'])
-        
+            out += "%d %s\n" % (s['id'], s['code'])        
         return out
-
 
     def format_assets(self, vals):
-        out = self.format_shots(vals)
-        
+        out = self.format_shots(vals)        
         return out
-
 
     def format_assetsandshots(self, vals):
-        out = self.format_shots(vals)
-            
+        out = self.format_shots(vals)           
         return out
-
 
     def format_version_fields(self, vals):
         out = ""
@@ -223,34 +207,20 @@ class OutputCmdLine (OutputDefault):
             out += "\n[%s]\n" % type
             for name in fields:
                 out += "%s\n" % name
-
         return out[1:-1]
-
 
     def format_version_statuses(self, vals):
         out = ""
         for s in vals:
             out += "%s\n" % (s)            
-
         return out[:-1]
-
 
     def format_version_name_templates(self, vals):
         out = ""
         out = "\n".join( [t.strip() for t in vals] )
         # if we want to strip the ${} out of it, this is the quick and dirty method.
         # return "\n".join( [t.replace("${","").replace("}","").strip() for t in templates] )
-
         return out
-
-
-    def format_scene_path_parse(self, vals):
-        out = ""
-        if vals:
-            out = "\n".join( [v.strip() for v in vals] )
-
-        return out
-
 
     def format_config(self, vals):
         out = ""
@@ -262,18 +232,12 @@ class OutputCmdLine (OutputDefault):
                 out += "\n"
         return out[1:-1]
 
-
     def format_version_create(self, vals):
-        out = vals['id']
-        
+        out = vals['id']       
         return out
 
 
     def format_version_update(self, vals):
-        out = vals['id']
-        
+        out = vals['id']        
         return out
-
-
-
 
